b0c1f25deca0980ef72aa8f77d9a05f95808d699
Széles Máté <szelessm@gmail.com>
2021-11-04 10:45:58 +0100

    add subscribe()

--- paymentSystem.js
+++ paymentSystem.js
@@ -3,4 +3,12 @@ export class PaymentSystem {
         console.log('...');
         return paymentProviderFunction(amount);
     }
+
+    subscribe(amount, paymentProviderFunction, numMonths) {
+        let sum = 0;
+        for(let i = 0; i < numMonths; i++) {
+            sum += paymentProviderFunction(amount);
+        }
+        return sum;
+    }
 }
