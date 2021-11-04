c4a47c9f682aeb1a6721e2e2fd718ea22ad08284
Széles Máté <szelessm@gmail.com>
2021-11-04 10:33:44 +0100

    complete the first test block

--- test.js
+++ test.js
@@ -10,5 +10,9 @@ describe('Test for PaymentSytem class', () => {
 
     test('Testing pay() function', () => {
        expect(myPaymentSystem.pay(500, paymentFn)).toBe(500);
+       expect(paymentFn).toHaveBeenCalled();
+       expect(paymentFn).toHaveBeenCalledWith(500);
+       expect(paymentFn.mock.calls.length).toBe(1);
+       expect(paymentFn.mock.calls[0][0]).toBe(500);
     });
 });
